4b4f5a024fd52eb77a6a42d930f8937b0c9f0508
Flavio Corsetty <flavio.corsetti@yahoo.it>
2024-05-21 11:55:44 +0200

    primo esercizio

--- HW4-FlavioCorsetti.hs
+++ HW4-FlavioCorsetti.hs
@@ -1,4 +1,5 @@
 import System.IO
+import Data.Char 
 
 main :: IO()
 main = do
@@ -6,35 +7,33 @@ main = do
 --- TEST AREA ---
 
     charCount
-
+    
 -----------------
-
 
 -- 1. Input/Output
 
-{-
 charCount :: IO()
 charCount = do 
         putStrLn "Inserisci un numero: "
         number <- getLine
         let num = read number :: Int
-        count <- getString num
-        putStrLn ("Numero di stringhe con ogni lettera:" ++ count )
+        count <- getString num [(chr x, 0) | x <- [97..122]]
+        putStrLn ("Numero di occorrenze di ogni lettera: \n" ++ show count )
 
-getString :: Int -> IO Int 
-getString 0 = return 0
-getString n = do
+getString :: Int -> [(Char, Int)] -> IO [(Char, Int)]
+getString 0 xs = return xs
+getString n xs = do
         putStrLn "Inserisci una stringa: "
         string <- getLine
-        contaLettere string + getString n-1
+        getString (n-1) (contaLettere (map toLower string) xs)
 
-contaLettere :: String -> IO Int
-contaLettere s
-    | conteggio = return  1
-    | otherwise = return 0
-    where 
-        conteggio = all (`elem` s) ['a' .. 'z']
-        -}
+contaLettere :: String -> [(Char, Int)] -> [(Char, Int)]
+contaLettere str xs = [(char, if char `elem` str then occ+1 else occ) | (char, occ) <- xs]
+    
+        
+{-
+
+
 
 charCount :: IO ()
 charCount = do 
@@ -60,3 +59,4 @@ contaLettere s
     | otherwise = return 0
     where 
         conteggio = all (`elem` s) ['a' .. 'z']
+        -}
